66e4155238193322f85b6814bfe025f198d7f083
Nattot <danlexder@gmail.com>
2023-04-13 10:09:47 +0700

    Revert "please commit"
    This reverts commit d031400f004ed5597a0921f28a5f12e9a6f12ec5.

--- components/OnboardingItem.js
+++ components/OnboardingItem.js
@@ -7,11 +7,12 @@ export default function OnboardingItem({ item }) {
 
   return (
     <View style={[styles.container, { width }]}>
-      <Image source={item.image} style={[styles.image, { width, resizeMode: 'contain' }]} />
-      <View style={styles.content}>
+      <Image source={item.image} style={[styles.image, {width, resizeMode: 'contain'}]} />
+      <View style={{ flex: 0.3 }}>
         <Text style={styles.title}>{item.title}</Text>
         <Text style={styles.description}>{item.description}</Text>
       </View>
+
     </View>
   );
 }
@@ -19,19 +20,12 @@ export default function OnboardingItem({ item }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 500,
-    top: 150,
+    justifyContent: "center",
+    alignItems: "center",
   },
   image: {
-    flex: 0.5,
+    // flex: 1,
     justifyContent: 'center',
-  },
-  content: {
-    flex: 0.3,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   title: {
     fontWeight: '400',
