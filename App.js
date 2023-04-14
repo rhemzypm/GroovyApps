43bc891cf693855470fdca85689b4876e5728289
rhemzypm <rhemzy15@gmail.com>
2023-04-14 09:44:55 +0700

    raw

--- App.js
+++ App.js
@@ -7,32 +7,22 @@ import Splashscreen from './screens/Splashscreen';
 import SignInScreen from './components/SignInScreen';
 import InputOTP from './components/InputOTP';
 import Register from './components/Register';
-import BottomTab from './components/AnimatedScrollView';
-import TabNavigator from './app/contexts/TabBarProvider';
-import TabBarProvider from './app/contexts/TabBarProvider';
-import Home from './components/Home';
-import Create from './components/Create';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TabBarProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Create"
+          initialRouteName="SplashScreen"
           screenOptions={{headerShown: false}}>
           <Stack.Screen name="Splashscreen" component={Splashscreen} />
           <Stack.Screen name="Onboarding" component={Onboarding} />
           <Stack.Screen name="SignInScreen" component={SignInScreen} />
           <Stack.Screen name="InputOTP" component={InputOTP} />
           <Stack.Screen name="Register" component={Register} />
-          <Stack.Screen name="BottomTab" component={BottomTab} />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Create" component={Create} />
         </Stack.Navigator>
-        <TabNavigator />
       </NavigationContainer>
-    </TabBarProvider>
   )
 }
 
