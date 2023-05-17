7bf66ced2e082fe93c1da6995a8f3edaaeec73c9
rhemzypm <rhemzy15@gmail.com>
2023-05-17 09:40:54 +0700

    satset

--- components/data.js
+++ components/data.js
@@ -1,16 +1,16 @@
 const data = [
   {
-    title: "Santai Masbro",
-    body: "Santai Masbro",
-    imgUrl: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2023/02/07/2709288676.jpg",
+    title: "Jusuf Kalla Nyalon",
+    body: "Dalam sebuah acara Halal bi Halal..",
+    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/JK-Sebut-Ekonomi-Indonesia-Dikuasai-China-Benarkah.jpeg?w=1000&ssl=1",
   },
   {
-    title: "Skip",
-    body: "ngawur kamu masbro",
-    imgUrl: "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2023/02/10/785646946.jpg",
+    title: "Vice Media Bangkrut",
+    body: "Para kreditur memperoleh Vice...",
+    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/Vice-Media-Has-Acquired-Refinery29-for-400-Million-USD.jpg?w=480&ssl=1",
   },
   {
-    title: "M A S    B R O",
-    body: "sukses selalu masbro",    
-    imgUrl: "https://assets.ayobandung.com/crop/0x0:0x0/750x500/webp/photo/2023/02/05/1513278662.jpg",  },];
+    title: "Platform AI dari IBM",
+    body: "Watsonx adalah AI dan...",    
+    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/IBM-Segera-Luncurkan-Watsonx-source-by-Mashable.jpeg?resize=1155%2C770&ssl=1",  },];
 export default data;
