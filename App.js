b3037b7f01c487bc30142d1174f5cd0a567e2025
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-23 12:04:32 +0700

    Updated app

--- App.js
+++ App.js
@@ -1,97 +1,100 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import Onboarding from './screens/Onboarding';
-import Splashscreen from './screens/Splashscreen';
-import SignInScreen from './screens/SignInScreen';
-import InputOTP from './components/InputOTP';
-import Register from './screens/Register';
-import BottomNav from './components/BottomNav';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import PointScreen from './screens/PointScreen';
-import FAQScreen from './screens/FAQScreen';
-import TabBar from './components/TabBar';
-import Profile from './screens/Profile';
-import ProductScreen from './screens/ProductScreen';
-import MessageScreen from './screens/messages/MessageScreen';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import Onboarding from "./screens/Onboarding";
+import Splashscreen from "./screens/Splashscreen";
+import SignInScreen from "./screens/SignInScreen";
+import InputOTP from "./components/InputOTP";
+import Register from "./screens/Register";
+import BottomNav from "./components/BottomNav";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import PointScreen from "./screens/PointScreen";
+import FAQScreen from "./screens/FAQScreen";
+import TabBar from "./components/TabBar";
+import Profile from "./screens/Profile";
+import ProductScreen from "./screens/ProductScreen";
+import MessageScreen from "./screens/messages/MessageScreen";
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 //test
-import { SafeAreaView} from 'react-native';
-import CarouselCards from './components/CarouselCards';
+import { SafeAreaView } from "react-native";
+import CarouselCards from "./components/CarouselCards";
 
-function TabNavigator(){
-  return <Tab.Navigator
-    tabBar={props => <TabBar {...props}/>}
-    initialRouteName="Home"
-    screenOptions={{headerShown: false}}>
-        <Tab.Screen 
-        name='Home' 
-        component={HomeScreen} 
-        initialParams={{icon: "home" }}
-        />
-        <Tab.Screen 
-        name='Point' 
-        component={PointScreen} 
-        initialParams={{icon: "point" }}
-        />
-      <Tab.Screen 
-      name='Product' 
-      component={ProductScreen} 
-      initialParams={{icon: "shoppingcart" }}
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => <TabBar {...props} />}
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        initialParams={{ icon: "home" }}
       />
-        <Tab.Screen 
-        name='Help' 
-        component={FAQScreen} 
-        initialParams={{icon: "customerservice" }}
-        />
-      <Tab.Screen 
-      name='Profile' 
-      component={Profile} 
-      initialParams={{icon: "user" }}
+      <Tab.Screen
+        name="Point"
+        component={PointScreen}
+        initialParams={{ icon: "point" }}
       />
-        </Tab.Navigator>
-      }
-
+      <Tab.Screen
+        name="Product"
+        component={ProductScreen}
+        initialParams={{ icon: "shoppingcart" }}
+      />
+      <Tab.Screen
+        name="Help"
+        component={FAQScreen}
+        initialParams={{ icon: "customerservice" }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        initialParams={{ icon: "user" }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 function StackNavigator() {
   return (
     <Stack.Navigator
-    initialRouteName="Register"
-    screenOptions={{headerShown: false}}>
-    <Stack.Screen name="Splashscreen" component={Splashscreen} />
-    <Stack.Screen name="Onboarding" component={Onboarding} />
-    <Stack.Screen name="SignInScreen" component={SignInScreen} />
-    <Stack.Screen name="InputOTP" component={InputOTP} />
-    <Stack.Screen name="Register" component={Register} />
-    <Stack.Screen name="BottomNav" component={BottomNav} />
-    <Stack.Screen name="CarouselCards" component={CarouselCards} />
-    <Stack.Screen name="MessageScreen" component={MessageScreen} />
-    <Stack.Screen
-          name="TabNavigator"
-          component={TabNavigator}
-          options={{ headerShown: false}}
-        />
+      initialRouteName="Register"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Splashscreen" component={Splashscreen} />
+      <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="SignInScreen" component={SignInScreen} />
+      <Stack.Screen name="InputOTP" component={InputOTP} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="BottomNav" component={BottomNav} />
+      <Stack.Screen name="CarouselCards" component={CarouselCards} />
+      <Stack.Screen name="MessageScreen" component={MessageScreen} />
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
-    );
-  }
+  );
+}
 
-  export default function App() {
-    return (
+export default function App() {
+  return (
     <NavigationContainer>
-    <StackNavigator />
+      <StackNavigator />
     </NavigationContainer>
-    );
-  }
-  
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center', 
-    justifyContent: 'center',
-  }
-})
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
