8743001774407eb0dd29ff0ad9f8d2b2d76e2a2c
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-23 12:03:59 +0700

    Updated data

--- components/data.js
+++ components/data.js
@@ -1,16 +1,21 @@
 const data = [
   {
-    title: "Jusuf Kalla Nyalon",
-    body: "Dalam sebuah acara Halal bi Halal..",
-    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/JK-Sebut-Ekonomi-Indonesia-Dikuasai-China-Benarkah.jpeg?w=1000&ssl=1",
+    postTitle: "Jusuf Kalla Nyalon",
+    postDescription: "Dalam sebuah acara Halal bi Halal..",
+    postImage:
+      "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/JK-Sebut-Ekonomi-Indonesia-Dikuasai-China-Benarkah.jpeg?w=1000&ssl=1",
   },
   {
-    title: "Vice Media Bangkrut",
-    body: "Para kreditur memperoleh Vice...",
-    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/Vice-Media-Has-Acquired-Refinery29-for-400-Million-USD.jpg?w=480&ssl=1",
+    postTitle: "Vice Media Bangkrut",
+    postDescription: "Para kreditur memperoleh Vice...",
+    postImage:
+      "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/Vice-Media-Has-Acquired-Refinery29-for-400-Million-USD.jpg?w=480&ssl=1",
   },
   {
-    title: "Platform AI dari IBM",
-    body: "Watsonx adalah AI dan...",    
-    imgUrl: "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/IBM-Segera-Luncurkan-Watsonx-source-by-Mashable.jpeg?resize=1155%2C770&ssl=1",  },];
+    postTitle: "Platform AI dari IBM",
+    postDescription: "Watsonx adalah AI dan...",
+    postImage:
+      "https://i0.wp.com/andalpost.com/wp-content/uploads/2023/05/IBM-Segera-Luncurkan-Watsonx-source-by-Mashable.jpeg?resize=1155%2C770&ssl=1",
+  },
+];
 export default data;
