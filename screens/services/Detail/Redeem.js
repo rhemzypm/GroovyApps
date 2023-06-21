57139cc3b2be8a9df23bd1ede4175ca45894944a
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-06-21 15:29:08 +0700

    Updated redeem page

--- screens/services/Detail/Redeem.js
+++ screens/services/Detail/Redeem.js
@@ -1,30 +1,41 @@
 import React from "react";
-import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { TokenData } from "../../../components/services/TokenData";
 import { useNavigation } from "@react-navigation/native";
-import Lottie from 'lottie-react-native';
+import Lottie from "lottie-react-native";
 
 const screenWidth = Dimensions.get("window").width;
 
 const Redeem = () => {
-
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.lottie}>
-        <Lottie 
-            source={require('../../../assets/img/present.json')} 
-            autoPlay
-            loop={false}
-            style={{ 
-        }} 
+        <Lottie
+          source={require("../../../assets/img/present.json")}
+          autoPlay
+          loop={false}
+          style={{}}
         />
       </View>
       <Text style={styles.header}>Redeem Successful!</Text>
-        <Text style={styles.text}>Your voucher has been successfully redeemed</Text>
-        <Text style={styles.text}>check your e-mail to use your voucher code</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Point')}>
-      <Text style={styles.buttonText}>Back</Text>
+      <Text style={styles.text}>
+        Your voucher has been successfully redeemed
+      </Text>
+      <Text style={styles.text}>
+        check your e-mail to use your voucher code
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Point")}
+      >
+        <Text style={styles.buttonText}>Back</Text>
       </TouchableOpacity>
     </View>
   );
@@ -42,7 +53,7 @@ const styles = {
     width: 330,
     height: 280,
   },
-  header:{
+  header: {
     fontSize: 25,
     marginBottom: 20,
   },
@@ -56,7 +67,7 @@ const styles = {
     borderRadius: 10,
     elevation: 5,
     width: screenWidth * 0.4,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
     color: "black",
