c100e591527df4d3471a9f1f732d3eea01f27e07
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-14 15:07:17 +0700

    Updated product

--- mobile/components/ProductBox.js
+++ mobile/components/ProductBox.js
@@ -25,7 +25,7 @@ const ProductBox = ({ packageData, onPress }) => {
           <Image
             source={{
               uri: `${packageData.packageImage.replace(
-                "http://127.0.0.1:5000/v1/ga/",
+                "http://localhost:5000/v1/ga/",
                 BACKEND_URL
               )}`,
             }}
--- mobile/components/ProductDetailsContainer.js
+++ mobile/components/ProductDetailsContainer.js
@@ -23,7 +23,7 @@ const ProductDetailsContainer = ({ packageData }) => {
           <Image
             source={{
               uri: `${packageData.packageImage.replace(
-                "http://127.0.0.1:5000/v1/ga/",
+                "http://localhost:5000/v1/ga/",
                 BACKEND_URL
               )}`,
             }}
