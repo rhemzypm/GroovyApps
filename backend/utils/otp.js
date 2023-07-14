8e103a09f6e1b26e455beedb81bb44ffaf923aef
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-07-14 11:13:01 +0700

    Removed backend folder

--- backend/utils/otp.js
+++
@@ -1,9 +0,0 @@
-exports.generateOTP = (otpLength) => {
-  const digits = '0123456789';
-  let OTP = '';
-
-  for (let i = 0; i < otpLength; i++) {
-    OTP += digits[Math.floor(Math.random() * 10)];
-  }
-  return OTP;
-};
